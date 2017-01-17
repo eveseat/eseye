ed95d4ee9f54f2416ec051b3de19b1167a57fc4d
Leon Jacobs <leonja511@gmail.com>
2017-01-17 10:39:04 +0200

    Vendor the tokengenerator bin and version bump

--- src/Eseye.php
+++ src/Eseye.php
@@ -43,7 +43,7 @@ class Eseye
     /**
      * The Eseye Version.
      */
-    const VERSION = '0.0.2';
+    const VERSION = '0.0.3';
 
     /**
      * @var \Seat\Eseye\Containers\EsiAuthentication
