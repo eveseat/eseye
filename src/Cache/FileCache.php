c8c0d9f726e473ea28d0acae41403f5a7ddce8ad
Maikel van der Knaap / Jarno Midumulf <contact@salox.nl>
2017-01-27 09:30:40 +0100

    styleci fix

--- src/Cache/FileCache.php
+++ src/Cache/FileCache.php
@@ -175,7 +175,7 @@ class FileCache implements CacheInterface
 
         $path = $this->buildRelativePath($uri, $query);
         $cache_file_path = $path . $this->results_filename;
-        
+
         @unlink($cache_file_path);
     }
 
