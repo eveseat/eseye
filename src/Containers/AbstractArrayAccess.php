8f93e485c5504118e63b1a430d15d43f109e3121
Leon Jacobs <leonja511@gmail.com>
2018-04-17 18:31:03 +0000

    Apply fixes from StyleCI

--- src/Containers/AbstractArrayAccess.php
+++ src/Containers/AbstractArrayAccess.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of SeAT
  *
- * Copyright (C) 2015, 2016, 2017  Leon Jacobs
+ * Copyright (C) 2015, 2016, 2017, 2018  Leon Jacobs
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
