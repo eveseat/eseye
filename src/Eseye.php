e157ec0394241c8844f230e7167c21c9ff6afdb4
Leon Jacobs <leonja511@gmail.com>
2018-05-27 10:13:33 +0200

    v1.1.5 🎉

--- src/Eseye.php
+++ src/Eseye.php
@@ -45,7 +45,7 @@ class Eseye
     /**
      * The Eseye Version.
      */
-    const VERSION = '1.1.4';
+    const VERSION = '1.1.5';
 
     /**
      * @var \Seat\Eseye\Containers\EsiAuthentication
