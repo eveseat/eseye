8f0f74041bc22eda7f246b69efd08fa4fa92af23
Leon Jacobs <leonja511@gmail.com>
2020-04-11 09:11:31 +0000

    Apply fixes from StyleCI

--- src/Checker/Header/TypeChecker.php
+++ src/Checker/Header/TypeChecker.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of SeAT
  *
- * Copyright (C) 2015, 2016, 2017, 2018, 2019, 2020  Leon Jacobs
+ * Copyright (C) 2015 to 2020 Leon Jacobs
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
