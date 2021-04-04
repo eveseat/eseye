69a0180f1727369614971fe377a1a8b2301c505d
Loïc Leuilliot <loic.leuilliot@gmail.com>
2021-04-04 22:29:19 +0200

    build(core): version bump

--- src/Eseye.php
+++ src/Eseye.php
@@ -45,7 +45,7 @@ class Eseye
     /**
      * The Eseye Version.
      */
-    const VERSION = '2.3.1';
+    const VERSION = '2.3.2';
 
     /**
      * @var \Seat\Eseye\Containers\EsiAuthentication
