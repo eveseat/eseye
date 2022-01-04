2a1b900d8e9324ba86df074eb0dcf34f22979e30
Leon Jacobs <leonja511@gmail.com>
2022-01-04 16:24:51 +0200

    Apply fixes from StyleCI (#79)
    Co-authored-by: StyleCI Bot <bot@styleci.io>

--- src/Access/AccessInterface.php
+++ src/Access/AccessInterface.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of SeAT
  *
- * Copyright (C) 2015 to 2021 Leon Jacobs
+ * Copyright (C) 2015 to 2022 Leon Jacobs
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
